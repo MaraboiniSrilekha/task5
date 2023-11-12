1cbd8ef219ce6ce41d904cf0b081f290d1e225b6
srilekha yadav maraboini <150052882+MaraboiniSrilekha@users.noreply.github.com>
2023-11-12 23:04:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("welcome guys")
+var num1=3
+var num2=4
+var num3=8
+console.log(num1+num2)
+
+
+i_favourate_food="pizza"
+myNativePlace="kurnool"
+numOfBirds=27
+
+ var num2=8
+console.log(num1+num2)
+
+console.log(num1*num2)
+console.log(num3/num2)
+
+num4=4
+num4++
+console.log(num4)
+num4--
+console.log(num4)
+
+let num5=4
+   num5=num5+6
+   console.log(num5)
+
+   
